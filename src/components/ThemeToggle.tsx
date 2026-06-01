41c189870e55060d9c408c9768b75c365fd1515b
music6745-max <music6745@gmail.com>
2026-06-01 19:56:05 +0900

    chore: tag P78 starter pack links

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -51,7 +51,8 @@ export function ThemeToggle() {
                   itemId === "8383387" ||
                   itemId === "8383391" ||
                   itemId === "8383396" ||
-                  itemId === "8383402"
+                  itemId === "8383402" ||
+                  itemId === "8383405"
                 ? "starter_pack"
                 : "single_template";
           url.searchParams.set("utm_source", "net-toolbox");
